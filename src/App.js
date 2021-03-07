aa076ab6be11d9781a1fa2bae8765af5552c2833
TheKetan2 <ketantoo@gmail.com>
2021-03-07 09:38:36 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ export default class App extends Component {
   }
 
   handleInput(event) {
-    // console.log(event.target.value);
     this.setState(
       {
         uname: event.target.value
@@ -71,24 +70,58 @@ const Formdata = ({ udata }) => {
     console.log(JSON.stringify(udata.login));
   }, [udata]);
 
-  return udata ? (
-    <div>
-      <img
-        style={{ width: 100, height: 100 }}
-        className="imge"
-        src={udata?.avatar_url}
-        alt="  "
-      />
-      <div className="details">
-        <div className="compon">Followers: {udata?.followers}</div>
-        <div className="compon">Following: {udata?.following}</div>
-        <div className="compon">public repos: {udata?.public_repos}</div>
-      </div>
-      <div className="urls">Page:{udata?.url}</div>
-    </div>
-  ) : (
-    <div>
-      <p>No Data Available</p>
+  return (
+    <div style={styles.card}>
+      {udata.login ? (
+        <div style={styles.cardImg}>
+          <div>
+            <img
+              style={styles.img}
+              className="imge"
+              src={udata?.avatar_url}
+              alt="  "
+            />
+          </div>
+          <div className="details">
+            <div className="compon">Followers: {udata?.followers}</div>
+            <div className="compon">Following: {udata?.following}</div>
+            <div className="compon">Public repos: {udata?.public_repos}</div>
+
+            <div className="urls">Page: {udata?.url}</div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <p>No Data Available</p>
+        </div>
+      )}
     </div>
   );
+};
+
+const styles = {
+  card: {
+    display: "flex",
+    flex: 1,
+    backgroundColor: "white",
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 5
+  },
+  cardImg: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    color: "rgba(0,0,0,0.7)"
+  },
+  img: {
+    marginRight: 10,
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    overflow: "hidden"
+  }
 };
